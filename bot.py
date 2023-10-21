4ab03424c8dc7e7650f7953273d5c6fc2addfd96
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2023-10-21 19:12:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
-from, Script import script
+from Script import script
 
 
 from plugins.webcode import bot_run
