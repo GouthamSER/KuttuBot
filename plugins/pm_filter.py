4167ba9e84033cfebe33bcb351df5cd66db5478b
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2023-10-21 19:18:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -711,7 +711,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"({script.RESULT_TXT}")
+        cap = f"({script.RESULT_TXT})"
     if imdb and imdb.get('poster'):
         try:
             await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
