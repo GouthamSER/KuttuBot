3c668da6d4770af492f8c3536cb93ff8b5794313
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2024-10-23 23:08:32 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -125,8 +125,14 @@ FɪʟᴇSɪᴢᴇ : {file_size}
 
     
     RESTART_TXT = """
-<b>Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ
-@im_goutham_josh</b>"""
+<b>𝖡𝗈𝗍 𝖱𝖾𝗌𝗍𝖺𝗋𝗍𝖾𝖽 !</b>
+Kuttu Bot
+**@im_goutham_josh**
+
+📅 𝖣𝖺𝗍𝖾 : <code>{}</code>
+⏰ 𝖳𝗂𝗆𝖾 : <code>{}</code>
+🌐 𝖳𝗂𝗆𝖾𝗓𝗈𝗇𝖾 : <code>Asia/Kolkata</code>
+🛠️ 𝖡𝗎𝗂𝗅𝖽 𝖲𝗍𝖺𝗍𝗎𝗌 : <code>𝗏1 [ 𝖲𝗍able 😁 ]</code></b>"""
     
     LOG_TEXT_P = """#NewUser
 ID - <code>{}</code>
