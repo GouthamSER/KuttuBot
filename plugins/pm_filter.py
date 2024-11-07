dfeff34950a7af02d90d5a7cafdf53633a8d3b66
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2024-11-07 18:26:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -714,17 +714,25 @@ async def auto_filter(client, msg, spoll=False):
         cap = script.RESULT_TXT.format(search) #result for group
     if imdb and imdb.get('poster'):
         try:
-            await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
+            delauto = await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
                                       reply_markup=InlineKeyboardMarkup(btn))
+                      await asyncio.sleep(600)
+                      await delauto.delete() #del msg auto 10min filter
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
-            await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
+            delau = await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
+                    await asyncio.sleep(600)
+                    await delau.delete()#del msg auto 10min filter
         except Exception as e:
             logger.exception(e)
-            await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
+            audel = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
+                    await asyncio.sleep(600)
+                    await audel.delete()#del msg auto 10min filter
     else:
-        await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
+        autodel = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
+                  await asyncio.sleep(600)
+                  await autodel.delete()#del msg auto 10min filter
     if spoll:
         await msg.message.delete()
 
