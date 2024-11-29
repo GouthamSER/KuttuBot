745f68efed7254326ad479d033a9c508b4650e10
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2024-11-29 15:18:22 +0530

    Add files via upload

--- database/connections_mdb.py
+++ database/connections_mdb.py
@@ -1,194 +1,133 @@
 import pymongo
-from sample_info import tempDict
-from info import DATABASE_URI, DATABASE_NAME, SECONDDB_URI
-import logging
 
+from info import DATABASE_URI, DATABASE_NAME
+
+import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
-# Connect to the first database
 myclient = pymongo.MongoClient(DATABASE_URI)
 mydb = myclient[DATABASE_NAME]
-mycol = mydb['CONNECTION']
-
-# Connect to the second database
-myclient2 = pymongo.MongoClient(SECONDDB_URI)
-mydb2 = myclient2[DATABASE_NAME]
-mycol2 = mydb2['CONNECTION']
+mycol = mydb['CONNECTION']   
 
 
-# Add a connection to the correct database
 async def add_connection(group_id, user_id):
-    # Select the correct collection based on tempDict
-    if tempDict['indexDB'] == DATABASE_URI:
-        mycol_to_use = mycol
-    else:
-        mycol_to_use = mycol2
-
-    query = mycol_to_use.find_one(
+    query = mycol.find_one(
         { "_id": user_id },
         { "_id": 0, "active_group": 0 }
     )
-    
     if query is not None:
         group_ids = [x["group_id"] for x in query["group_details"]]
         if group_id in group_ids:
             return False
 
     group_details = {
-        "group_id": group_id
+        "group_id" : group_id
     }
 
     data = {
         '_id': user_id,
-        'group_details': [group_details],
-        'active_group': group_id,
+        'group_details' : [group_details],
+        'active_group' : group_id,
     }
 
-    if mycol.count_documents({"_id": user_id}) == 0 and mycol2.count_documents({"_id": user_id}) == 0:
+    if mycol.count_documents( {"_id": user_id} ) == 0:
         try:
-            mycol_to_use.insert_one(data)
+            mycol.insert_one(data)
             return True
         except:
             logger.exception('Some error occurred!', exc_info=True)
-            return False
+
     else:
         try:
-            mycol_to_use.update_one(
+            mycol.update_one(
                 {'_id': user_id},
                 {
                     "$push": {"group_details": group_details},
-                    "$set": {"active_group": group_id}
+                    "$set": {"active_group" : group_id}
                 }
             )
             return True
         except:
             logger.exception('Some error occurred!', exc_info=True)
-            return False
 
-
-# Get the active connection for a user
+        
 async def active_connection(user_id):
-    # Select the correct collection based on tempDict
-    if tempDict['indexDB'] == DATABASE_URI:
-        mycol_to_use = mycol
-    else:
-        mycol_to_use = mycol2
 
-    query = mycol_to_use.find_one(
+    query = mycol.find_one(
         { "_id": user_id },
         { "_id": 0, "group_details": 0 }
     )
-    
     if not query:
         return None
-    
+
     group_id = query['active_group']
-    return int(group_id) if group_id is not None else None
+    return int(group_id) if group_id != None else None
 
 
-# Get all connections for a user
 async def all_connections(user_id):
-    # Select the correct collection based on tempDict
-    if tempDict['indexDB'] == DATABASE_URI:
-        mycol_to_use = mycol
-    else:
-        mycol_to_use = mycol2
-
-    query = mycol_to_use.find_one(
+    query = mycol.find_one(
         { "_id": user_id },
         { "_id": 0, "active_group": 0 }
     )
-    
     if query is not None:
         return [x["group_id"] for x in query["group_details"]]
     else:
         return None
 
 
-# Check if a user is active in a particular group
 async def if_active(user_id, group_id):
-    # Select the correct collection based on tempDict
-    if tempDict['indexDB'] == DATABASE_URI:
-        mycol_to_use = mycol
-    else:
-        mycol_to_use = mycol2
-
-    query = mycol_to_use.find_one(
+    query = mycol.find_one(
         { "_id": user_id },
         { "_id": 0, "group_details": 0 }
     )
-    
     return query is not None and query['active_group'] == group_id
 
 
-# Make a user active in a particular group
 async def make_active(user_id, group_id):
-    # Select the correct collection based on tempDict
-    if tempDict['indexDB'] == DATABASE_URI:
-        mycol_to_use = mycol
-    else:
-        mycol_to_use = mycol2
-
-    update = mycol_to_use.update_one(
+    update = mycol.update_one(
         {'_id': user_id},
-        {"$set": {"active_group": group_id}}
+        {"$set": {"active_group" : group_id}}
     )
-    
     return update.modified_count != 0
 
 
-# Make a user inactive
 async def make_inactive(user_id):
-    # Select the correct collection based on tempDict
-    if tempDict['indexDB'] == DATABASE_URI:
-        mycol_to_use = mycol
-    else:
-        mycol_to_use = mycol2
-
-    update = mycol_to_use.update_one(
+    update = mycol.update_one(
         {'_id': user_id},
-        {"$set": {"active_group": None}}
+        {"$set": {"active_group" : None}}
     )
-    
     return update.modified_count != 0
 
 
-# Delete a user's connection to a specific group
 async def delete_connection(user_id, group_id):
-    # Select the correct collection based on tempDict
-    if tempDict['indexDB'] == DATABASE_URI:
-        mycol_to_use = mycol
-    else:
-        mycol_to_use = mycol2
 
     try:
-        update = mycol_to_use.update_one(
+        update = mycol.update_one(
             {"_id": user_id},
-            {"$pull": {"group_details": {"group_id": group_id}}}
+            {"$pull" : { "group_details" : {"group_id":group_id} } }
         )
-        
         if update.modified_count == 0:
             return False
-        
-        query = mycol_to_use.find_one(
+        query = mycol.find_one(
             { "_id": user_id },
             { "_id": 0 }
         )
-        
         if len(query["group_details"]) >= 1:
             if query['active_group'] == group_id:
                 prvs_group_id = query["group_details"][len(query["group_details"]) - 1]["group_id"]
-                mycol_to_use.update_one(
+
+                mycol.update_one(
                     {'_id': user_id},
-                    {"$set": {"active_group": prvs_group_id}}
+                    {"$set": {"active_group" : prvs_group_id}}
                 )
         else:
-            mycol_to_use.update_one(
+            mycol.update_one(
                 {'_id': user_id},
-                {"$set": {"active_group": None}}
+                {"$set": {"active_group" : None}}
             )
         return True
     except Exception as e:
         logger.exception(f'Some error occurred! {e}', exc_info=True)
         return False
+
--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -1,4 +1,3 @@
-
 import logging
 from struct import pack
 import re
@@ -8,17 +7,12 @@ from pymongo.errors import DuplicateKeyError
 from umongo import Instance, Document, fields
 from motor.motor_asyncio import AsyncIOMotorClient
 from marshmallow.exceptions import ValidationError
-from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER, MAX_B_TN, SECONDDB_URI
-from utils import get_settings, save_group_settings
-from sample_info import tempDict 
+from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-#some basic variables needed
-saveMedia = None
 
-#primary db
 client = AsyncIOMotorClient(DATABASE_URI)
 db = client[DATABASE_NAME]
 instance = Instance.from_db(db)
@@ -37,34 +31,6 @@ class Media(Document):
         indexes = ('$file_name', )
         collection_name = COLLECTION_NAME
 
-#secondary db
-client2 = AsyncIOMotorClient(SECONDDB_URI)
-db2 = client2[DATABASE_NAME]
-instance2 = Instance.from_db(db2)
-
-@instance2.register
-class Media2(Document):
-    file_id = fields.StrField(attribute='_id')
-    file_ref = fields.StrField(allow_none=True)
-    file_name = fields.StrField(required=True)
-    file_size = fields.IntField(required=True)
-    file_type = fields.StrField(allow_none=True)
-    mime_type = fields.StrField(allow_none=True)
-    caption = fields.StrField(allow_none=True)
-
-    class Meta:
-        indexes = ('$file_name', )
-        collection_name = COLLECTION_NAME
-
-async def choose_mediaDB():
-    """This Function chooses which database to use based on the value of indexDB key in the dict tempDict."""
-    global saveMedia
-    if tempDict['indexDB'] == DATABASE_URI:
-        logger.info("Using first db (Media)")
-        saveMedia = Media
-    else:
-        logger.info("Using second db (Media2)")
-        saveMedia = Media2
 
 async def save_file(media):
     """Save file in database"""
@@ -73,10 +39,7 @@ async def save_file(media):
     file_id, file_ref = unpack_new_file_id(media.file_id)
     file_name = re.sub(r"(_|\-|\.|\+)", " ", str(media.file_name))
     try:
-        if await Media.count_documents({'file_id': file_id}, limit=1):
-            logger.warning(f'{getattr(media, "file_name", "NO_FILE")} is already saved in primary DB !')
-            return False, 0
-        file = saveMedia(
+        file = Media(
             file_id=file_id,
             file_ref=file_ref,
             file_name=file_name,
@@ -91,7 +54,7 @@ async def save_file(media):
     else:
         try:
             await file.commit()
-        except DuplicateKeyError:  
+        except DuplicateKeyError:      
             logger.warning(
                 f'{getattr(media, "file_name", "NO_FILE")} is already saved in database'
             )
@@ -103,22 +66,9 @@ async def save_file(media):
 
 
 
-async def get_search_results(chat_id, query, file_type=None, max_results=10, offset=0, filter=False):
+async def get_search_results(query, file_type=None, max_results=10, offset=0, filter=False):
     """For given query return (results, next_offset)"""
-    if chat_id is not None:
-        settings = await get_settings(int(chat_id))
-        try:
-            if settings['max_btn']:
-                max_results = 10
-            else:
-                max_results = int(MAX_B_TN)
-        except KeyError:
-            await save_group_settings(int(chat_id), 'max_btn', False)
-            settings = await get_settings(int(chat_id))
-            if settings['max_btn']:
-                max_results = 10
-            else:
-                max_results = int(MAX_B_TN)
+
     query = query.strip()
     #if filter:
         #better ?
@@ -129,7 +79,7 @@ async def get_search_results(chat_id, query, file_type=None, max_results=10, off
     elif ' ' not in query:
         raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
     else:
-        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_()]')
+        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_]')
     
     try:
         regex = re.compile(raw_pattern, flags=re.IGNORECASE)
@@ -144,84 +94,30 @@ async def get_search_results(chat_id, query, file_type=None, max_results=10, off
     if file_type:
         filter['file_type'] = file_type
 
-    total_results = ((await Media.count_documents(filter))+(await Media2.count_documents(filter)))
+    total_results = await Media.count_documents(filter)
+    next_offset = offset + max_results
 
-    #verifies max_results is an even number or not
-    if max_results%2 != 0: #if max_results is an odd number, add 1 to make it an even number
-        logger.info(f"Since max_results is an odd number ({max_results}), bot will use {max_results+1} as max_results to make it even.")
-        max_results += 1
+    if next_offset > total_results:
+        next_offset = ''
 
     cursor = Media.find(filter)
-    cursor2 = Media2.find(filter)
     # Sort by recent
     cursor.sort('$natural', -1)
-    cursor2.sort('$natural', -1)
     # Slice files according to offset and max results
-    cursor2.skip(offset).limit(max_results)
+    cursor.skip(offset).limit(max_results)
     # Get list of files
-    fileList2 = await cursor2.to_list(length=max_results)
-    if len(fileList2)<max_results:
-        next_offset = offset+len(fileList2)
-        cursorSkipper = (next_offset-(await Media2.count_documents(filter)))
-        cursor.skip(cursorSkipper if cursorSkipper>=0 else 0).limit(max_results-len(fileList2))
-        fileList1 = await cursor.to_list(length=(max_results-len(fileList2)))
-        files = fileList2+fileList1
-        next_offset = next_offset + len(fileList1)
-    else:
-        files = fileList2
-        next_offset = offset + max_results
-    if next_offset >= total_results:
-        next_offset = ''
+    files = await cursor.to_list(length=max_results)
+
     return files, next_offset, total_results
 
-async def get_bad_files(query, file_type=None, filter=False):
-    """For given query return (results, next_offset)"""
-    query = query.strip()
-    #if filter:
-        #better ?
-        #query = query.replace(' ', r'(\s|\.|\+|\-|_)')
-        #raw_pattern = r'(\s|_|\-|\.|\+)' + query + r'(\s|_|\-|\.|\+)'
-    if not query:
-        raw_pattern = '.'
-    elif ' ' not in query:
-        raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
-    else:
-        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_()]')
-    
-    try:
-        regex = re.compile(raw_pattern, flags=re.IGNORECASE)
-    except:
-        return []
 
-    if USE_CAPTION_FILTER:
-        filter = {'$or': [{'file_name': regex}, {'caption': regex}]}
-    else:
-        filter = {'file_name': regex}
-
-    if file_type:
-        filter['file_type'] = file_type
-
-    cursor = Media.find(filter)
-    cursor2 = Media2.find(filter)
-    # Sort by recent
-    cursor.sort('$natural', -1)
-    cursor2.sort('$natural', -1)
-    # Get list of files
-    files = ((await cursor2.to_list(length=(await Media2.count_documents(filter))))+(await cursor.to_list(length=(await Media.count_documents(filter)))))
-
-    #calculate total results
-    total_results = len(files)
-
-    return files, total_results
 
 async def get_file_details(query):
     filter = {'file_id': query}
     cursor = Media.find(filter)
     filedetails = await cursor.to_list(length=1)
-    if not filedetails:
-        cursor2 = Media2.find(filter)
-        filedetails = await cursor2.to_list(length=1)
     return filedetails
+
 
 def encode_file_id(s: bytes) -> str:
     r = b""
