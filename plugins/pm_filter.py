c1969e9b9dd16d049a1a7ae870ef037dcc5aea9f
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2024-12-12 16:23:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -720,25 +720,21 @@ async def auto_filter(client, msg, spoll=False):
                                       reply_markup=InlineKeyboardMarkup(btn))
             await asyncio.sleep(150)
             await delauto.delete() #del msg auto 10min filter
-            await message.delete()
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
             delau = await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
             await asyncio.sleep(150)
             await delau.delete()#del msg auto 10min filter
-            await message.delete()
         except Exception as e:
             logger.exception(e)
             audel = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
             await asyncio.sleep(150)
             await audel.delete()#del msg auto 10min filter
-            await message.delete()
     else:
         autodel = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
         await asyncio.sleep(150)
         await autodel.delete()#del msg auto 10min filter
-        await message.delete()
     if spoll:
         await msg.message.delete()
 
