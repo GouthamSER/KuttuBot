46dc71f8111a4ca8c63f1ecb459844dae26d731a
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2025-04-22 12:16:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -5,7 +5,7 @@ import asyncio
 from Script import script
 from pyrogram import Client, filters, enums
 from pyrogram.errors import ChatAdminRequired, FloodWait
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup,CallbackQuery
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
 from database.users_chats_db import db
 from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT, FORCE_SUB1, FORCE_SUB2
