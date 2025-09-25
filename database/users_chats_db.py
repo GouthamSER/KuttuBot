d131a57e7d37c325097ff0214ace03f2471535e8
Goutham Josh 么 <97418751+GouthamSER@users.noreply.github.com>
2025-09-25 20:20:10 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -141,6 +141,24 @@ class Database:
 
     async def get_db_size(self):
         return (await self.db.command("dbstats"))['dataSize']
+    
+    
+    async def set_auth_channels(self, channels: list[int]):
+        # Store the list of auth channel IDs in a singleton document
+        await self.config.update_one(
+            {"_id": "auth_channels"},
+            {"$set": {"channels": channels}},
+            upsert=True,
+        )
+
+    async def get_auth_channels(self) -> list[int]:
+        doc = await self.config.find_one({"_id": "auth_channels"})
+        if doc and "channels" in doc:
+            return doc["channels"]
+        return []
+
+    async def get_db_size(self):
+        return (await self.db.command("dbstats"))['dataSize']
 
 
 db = Database(DATABASE_URI, DATABASE_NAME)
