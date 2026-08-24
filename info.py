d87cf165805a83ea3c468f2512623d60f9d4951a
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2026-08-24 15:40:37 +0530

    Replace TMDB_API_KEY with OMDB_API_KEY usage

--- info.py
+++ info.py
@@ -54,11 +54,11 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True)
-TMDB_API_KEY = environ.get("TMDB_API_KEY") #get from tmdb signup api get for  posters
+OMDB_API_KEY = environ.get("OMDB_API_KEY") #get free key from omdbapi.com, used for posters + metadata
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
-LOG_STR += "IMDb metadata via imdbio (no API key needed).\n"
+LOG_STR += ("OMDb metadata configured (OMDB_API_KEY set).\n" if OMDB_API_KEY else "OMDB_API_KEY not set, /imdb results will fail.\n")
 LOG_STR += ("P_TTI_SHOW_OFF found , Users will be redirected to send /start to Bot PM instead of sending file file directly\n" if P_TTI_SHOW_OFF else "P_TTI_SHOW_OFF is disabled files will be send in PM, instead of sending start.\n")
 LOG_STR += ("SINGLE_BUTTON is Found, filename and files size will be shown in a single button instead of two separate buttons\n" if SINGLE_BUTTON else "SINGLE_BUTTON is disabled , filename and file_sixe will be shown as different buttons\n")
 LOG_STR += (f"CUSTOM_FILE_CAPTION enabled with value {CUSTOM_FILE_CAPTION}, your files will be send along with this customized caption.\n" if CUSTOM_FILE_CAPTION else "No CUSTOM_FILE_CAPTION Found, Default captions of file will be used.\n")
