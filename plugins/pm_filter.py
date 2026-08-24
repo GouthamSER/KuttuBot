0b6afc2dfdd4ee5ba96d79cdf6391de3ef78a079
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2026-08-24 20:56:45 +0530

    Refactor InlineKeyboardButton creation for files

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -132,10 +132,10 @@ def _build_file_btn(files, settings, pre, key, offset, total_results, req):
         ]
     else:
         btn = [
-            [
-                InlineKeyboardButton(text=f"{file.file_name}",          callback_data=f"{pre}#{file.file_id}"),
-                InlineKeyboardButton(text=f"{get_size(file.file_size)}", callback_data=f"{pre}#{file.file_id}"),
-            ]
+            [InlineKeyboardButton(
+                text=f"{file.file_name} [{get_size(file.file_size)}]",
+                callback_data=f"{pre}#{file.file_id}"
+            )]
             for file in files
         ]
 
@@ -207,10 +207,10 @@ async def next_page(bot, query):
         ]
     else:
         btn = [
-            [
-                InlineKeyboardButton(text=f"{file.file_name}",          callback_data=f'files#{file.file_id}'),
-                InlineKeyboardButton(text=f"{get_size(file.file_size)}", callback_data=f'files_#{file.file_id}'),
-            ]
+            [InlineKeyboardButton(
+                text=f"{file.file_name} [{get_size(file.file_size)}]",
+                callback_data=f'files#{file.file_id}'
+            )]
             for file in files
         ]
 
@@ -984,10 +984,10 @@ async def auto_filter(client, msg, spoll=False):
         ]
     else:
         btn = [
-            [
-                InlineKeyboardButton(text=f"{file.file_name}",          callback_data=f'{pre}#{file.file_id}'),
-                InlineKeyboardButton(text=f"{get_size(file.file_size)}", callback_data=f'{pre}#{file.file_id}'),
-            ]
+            [InlineKeyboardButton(
+                text=f"{file.file_name} [{get_size(file.file_size)}]",
+                callback_data=f'{pre}#{file.file_id}'
+            )]
             for file in files
         ]
 
