542c7077910f2b83e985098284d082ccf57b4ab9
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2026-08-31 10:36:01 +0530

    Update IMDB template format in info.py

--- info.py
+++ info.py
@@ -46,7 +46,7 @@ SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', 'True')), False)
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CUSTOM_FILE_CAPTION}") ## CHANGE CODE IN SCRIPT
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", "📂 <em>File Name</em>: <code>{file_name}</code>\n\n ♻ <em>File Size</em>:{file_size} \n\n <b><i>Latest Movies -</i> [ELDORADO](https://t.me/wudixh) </b>")
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "180")) # seconds, 0 disables auto-delete
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "🏷 𝖳𝗂𝗍𝗅𝖾: <a href={url}>{title}</a> \n🔮 𝖸𝖾𝖺𝗋: {year} \n⭐️ 𝖱𝖺𝗍𝗂𝗇𝗀𝗌: {rating}/ 10 \n🎭 𝖦𝖾𝗇𝖾𝗋𝗌: {genres} \n\n🎊 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 [Kuttu Bot](https://t.me/im_kuttu_bot)")
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "🎬 <a href={url}>{title}</a> [{year}]{aka_line}\n⭐ IMDb Rating: <i>{rating} / 10</i>\n📅 Release Info: {release_date}\n🎭 Genre: {genre_tags}\n🌐 Language: {language_tags}\n🌍 Country of Origin: {country_tags}\n\n📖 Storyline:\n<i>{plot}</i>\n\n🎊 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 [Kuttu Bot](https://t.me/im_kuttu_bot)")
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
