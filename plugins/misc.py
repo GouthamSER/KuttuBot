17c84c0afc9d2b9dea62ce576ec0d2b0e4888f07
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2026-08-31 11:02:49 +0530

    Dead codes rmvd

--- plugins/misc.py
+++ plugins/misc.py
@@ -2,7 +2,7 @@ import os
 from pyrogram import Client, filters, enums
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from info import IMDB_TEMPLATE
-from utils import extract_user, get_file_id, get_poster, last_online
+from utils import extract_user, get_file_id, get_poster
 import time
 from datetime import datetime
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
